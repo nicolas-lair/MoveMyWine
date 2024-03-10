6edc4aa821f7836784781cddb8e1b438e0047db6
nlair <nl.nicolas.lair@gmail.com>
2024-03-10 19:26:44 +0100

    ruff formatting

--- test/my_transporters/stef/test_total_cost.py
+++ test/my_transporters/stef/test_total_cost.py
@@ -18,13 +18,36 @@ class TestTotalCost:
     def test_compute_cost(self):
         exp = SingleRefExpedition(n_bottles=30, bottle_type=BOTTLE, package=Package())
         dep = "75"
-        assert self.cost_calculator.compute_cost(gas_price=1., expedition=exp, department=dep) == 43.14+0.7
-        assert self.cost_calculator.compute_cost(gas_price=1.45, expedition=exp, department=dep) == 49.02
+        assert (
+            self.cost_calculator.compute_cost(
+                gas_price=1.0, expedition=exp, department=dep
+            )
+            == 43.14 + 0.7
+        )
+        assert (
+            self.cost_calculator.compute_cost(
+                gas_price=1.45, expedition=exp, department=dep
+            )
+            == 49.02
+        )
 
-        exp = MultiRefExpedition([
-            SingleRefExpedition(n_bottles=30, bottle_type=BOTTLE, package=Package()),
-            SingleRefExpedition(n_bottles=24, bottle_type=BOTTLE, package=Package())
-        ])
+        exp = MultiRefExpedition(
+            [
+                SingleRefExpedition(
+                    n_bottles=30, bottle_type=BOTTLE, package=Package()
+                ),
+                SingleRefExpedition(
+                    n_bottles=24, bottle_type=BOTTLE, package=Package()
+                ),
+            ]
+        )
         dep = "69"
-        assert self.cost_calculator.compute_cost(gas_price=1., expedition=exp, department=dep) == round(56.31+0.7, 2)
-        assert self.cost_calculator.compute_cost(gas_price=1.4, expedition=exp, department=dep) == 63.2
+        assert self.cost_calculator.compute_cost(
+            gas_price=1.0, expedition=exp, department=dep
+        ) == round(56.31 + 0.7, 2)
+        assert (
+            self.cost_calculator.compute_cost(
+                gas_price=1.4, expedition=exp, department=dep
+            )
+            == 63.2
+        )
