efa2d059fc36c0f84bcdebc61fb7ad4b2dede33f
nlair <nl.nicolas.lair@gmail.com>
2024-03-17 22:16:15 +0100

    Integrate postal code + reorganize UX

--- srcv2/streamlit_app.py
+++ srcv2/streamlit_app.py
@@ -9,69 +9,102 @@ from cost_calculator.expedition import SingleRefExpedition, MultiRefExpedition  
 from constant import BOTTLE, MAGNUM, Package  # noqa: E402
 from departement import DEPARTMENTS_TO_CODE  # noqa: E402
 from my_transporters.stef.gas_modulation_indicator import retrieve_indicator  # noqa: E402
+from app_generics.postal_code import get_postal_code_list  # noqa: E402
 
 st.title(":champagne: Move My Wine")
+if "cost" not in st.session_state:
+    st.session_state.cost = 0
 
-transporter = st.selectbox("Choix du transporteur", ["Stef"])
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("#### Transporteur")
+    transporter = st.selectbox(
+        "Choix du transporteur", ["Stef"], label_visibility="hidden"
+    )
+with col2:
+    st.text("")
+    result = st.empty()
+
+
+@st.cache_data
+def retrieve_postal_code():
+    return get_postal_code_list()
+
+
+if "department" not in st.session_state:
+    st.session_state.department = "75"
+
+
+def department_callback():
+    st.session_state.department = postal_code[0][:2]
+
+
+df_postal_code = retrieve_postal_code()
+
+st.markdown("#### Expédition")
 
 if transporter == "Stef":
     from my_transporters.stef.constant import TransporterParams
 
+    success_retrival, valid_date, current_gnr_indicator = retrieve_indicator()
     cost_calculator = StefTotalCost()
+
+    (
+        commune_col,
+        dept_col,
+    ) = st.columns([0.7, 0.3])
+    with commune_col:
+        postal_code = st.selectbox(
+            "Destination",
+            options=set(map(tuple, df_postal_code.values)),
+            format_func=lambda c: " - ".join(c),
+            on_change=department_callback,
+        )
+    with dept_col:
+        # TODO : Update department
+        st.text_input(
+            "Département",
+            value=DEPARTMENTS_TO_CODE[postal_code[0][:2]],
+            disabled=True,
+        )
 
     (
         col1,
         col2,
-        col3,
-    ) = st.columns([0.3, 0.3, 0.4])
+    ) = st.columns([0.25, 0.25])
     with col1:
         bottle = st.number_input(
-            "Nombre de bouteilles (75 cL)",
+            "Bouteilles (75 cL)",
             min_value=0,
             max_value=100,
             value="min",
             step=1,
         )
-        magnums = st.number_input(
-            "Nombre de magnums (1.5 L)", min_value=0, max_value=100, value="min", step=1
-        )
     with col2:
-        postal_code = st.selectbox(
-            "Code Postal",
-            options=DEPARTMENTS_TO_CODE.keys(),
-            format_func=lambda c: DEPARTMENTS_TO_CODE[c],
+        magnums = st.number_input(
+            "Magnums (1.5 L)", min_value=0, max_value=100, value="min", step=1
         )
-        destination = st.selectbox(
-            "Département",
-            options=DEPARTMENTS_TO_CODE.keys(),
-            format_func=lambda c: DEPARTMENTS_TO_CODE[c],
-        )
-    with col3:
-        success_retrival, valid_date, current_gnr_indicator = retrieve_indicator()
-        gas_modulation = st.number_input(
-            "Indice Coût GNR",
-            min_value=1.0,
-            max_value=2.0,
-            value=current_gnr_indicator,
-            format="%.4f",
-        )
-        if not success_retrival:
-            st.error(
-                "Indicateur GNR non récupéré [ici](%s) ! :disappointed:"
-                % TransporterParams.gas_modulation_link
-            )
-        elif not valid_date:
-            st.warning(
-                "Indicateur GNR à vérifier [ici](%s) ! :neutral_face:"
-                % TransporterParams.gas_modulation_link
-            )
-        else:
-            st.success(
-                "Indicateur GNR récupéré [ici](%s) ! :wink:"
-                % TransporterParams.gas_modulation_link
-            )
 
-    cost_detail = st.checkbox("Détails des coûts")
+    with st.expander("Surcharges", expanded=True):
+        col1, col2 = st.columns([0.2, 0.5])
+        with col2:
+            st.text("")
+            if not success_retrival:
+                st.error("Indicateur GNR non récupéré ! :disappointed:")
+            elif not valid_date:
+                st.warning("Indicateur GNR à vérifier ! :neutral_face:")
+            else:
+                st.success("Indicateur GNR récupéré !", icon="✅")
+        with col1:
+            gas_modulation = st.number_input(
+                "Indice Coût GNR - [Source](%s)"
+                % TransporterParams.gas_modulation_link,
+                min_value=1.0,
+                max_value=2.0,
+                value=current_gnr_indicator,
+                format="%.4f",
+                help="Récupéré automatiquement si possible",
+            )
 
     expedition = MultiRefExpedition(
         [
@@ -83,25 +116,41 @@ if transporter == "Stef":
             ),
         ]
     )
-    st.divider()
-    cost = cost_calculator.compute_cost(
+    detail_cost = cost_calculator.compute_cost(
         gas_price=gas_modulation,
         expedition=expedition,
-        department=destination,
-        return_details=cost_detail,
+        department=st.session_state.department,
+        return_details=True,
     )
-    st.markdown(
-        """
-    <style>
-    .big-font {
-        font-size:50px !important;
-        text-align: center
-    }
-    </style>
-    """,
-        unsafe_allow_html=True,
+    st.session_state.cost = cost_calculator.compute_cost(
+        gas_price=gas_modulation,
+        expedition=expedition,
+        department=st.session_state.department,
+        return_details=False,
     )
-    st.markdown(f'<p class="big-font">{cost} € HT</p>', unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown(
+            """
+        <style>
+        .big-font {
+            font-size:50px !important;
+            text-align: center
+        }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            f'<p class="big-font">{st.session_state.cost} € HT</p>',
+            unsafe_allow_html=True,
+        )
+        result.markdown(
+            f'<p class="big-font">{st.session_state.cost} € HT</p>',
+            unsafe_allow_html=True,
+        )
+    with col2:
+        st.write(detail_cost)
 
 else:
     st.write("Déso pas dispo")
