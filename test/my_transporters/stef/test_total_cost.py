7d561685c3ada7bba81a823c51d3ad7c78784542
nlair <nl.nicolas.lair@gmail.com>
2025-01-05 17:14:41 +0100

    Fix tests

--- test/my_transporters/stef/test_total_cost.py
+++ test/my_transporters/stef/test_total_cost.py
@@ -6,39 +6,23 @@ from src.constant import BOTTLE, Package
 class TestTotalCost:
     cost_calculator = StefTotalCost
 
-    # def test_init(self):
-    #     assert len(self.cost_calculator) == 3
-    #     assert isinstance(self.cost_calculator.gnr_modulator, ModulatorFromIndicator)
-    #     assert self.cost_calculator[CostType.ByBottle].gas_modulated
-    #     assert self.cost_calculator[CostType.Expedition].gas_modulated
-    #     assert not self.cost_calculator[CostType.Security].gas_modulated
-
     def test_compute_cost(self):
         exp = SingleRefExpedition(n_bottles=30, bottle_type=BOTTLE, package=Package())
         dep = "75"
-        assert (
-            self.cost_calculator.compute_cost(
-                cold_factor=300, gnr_factor=1.0, expedition=exp, department=dep
-            )
-            == 43.14 + 0.7
-        )
-        assert (
-            self.cost_calculator.compute_cost(
-                cold_factor=300, gnr_factor=1.45, expedition=exp, department=dep
-            )
-            == 49.02
-        )
-        assert (
-            self.cost_calculator.compute_cost(
-                cold_factor=315, gnr_factor=1.0, expedition=exp, department=dep
-            )
-            == 44.06
-        )
+        assert self.cost_calculator.compute_cost(
+            cold_factor=300, gnr_factor=1.0, expedition=exp, department=dep
+        ) == round((39.46 + 5.41) + 0.7, 2)
+        assert self.cost_calculator.compute_cost(
+            cold_factor=300, gnr_factor=1.45, expedition=exp, department=dep
+        ) == round((39.46 + 5.41) * 1.12 + 0.7, 2)
+        assert self.cost_calculator.compute_cost(
+            cold_factor=315, gnr_factor=1.0, expedition=exp, department=dep
+        ) == round((39.46 + 5.41) * 1.005 + 0.7, 2)
         assert (
             self.cost_calculator.compute_cost(
                 cold_factor=315, gnr_factor=1.45, expedition=exp, department=dep
             )
-            == 49.24
+            == 51.17
         )
 
         exp = MultiRefExpedition(
@@ -54,19 +38,16 @@ class TestTotalCost:
         dep = "69"
         assert self.cost_calculator.compute_cost(
             cold_factor=300, gnr_factor=1.0, expedition=exp, department=dep
-        ) == round(56.31 + 0.7, 2)
-        assert (
-            self.cost_calculator.compute_cost(
-                cold_factor=300, gnr_factor=1.4, expedition=exp, department=dep
-            )
-            == 63.2
-        )
+        ) == round((53.15 + 5.41) + 0.7, 2)
+        assert self.cost_calculator.compute_cost(
+            cold_factor=300, gnr_factor=1.4, expedition=exp, department=dep
+        ) == round((53.15 + 5.41) * 1.11 + 0.7, 2)
         assert self.cost_calculator.compute_cost(
             cold_factor=315, gnr_factor=1.0, expedition=exp, department=dep
-        ) == round(56.31 * 1.005 + 0.7, 2)
+        ) == round((53.15 + 5.41) * 1.005 + 0.7, 2)
         assert (
             self.cost_calculator.compute_cost(
                 cold_factor=315, gnr_factor=1.4, expedition=exp, department=dep
             )
-            == 63.48
+            == 65.99
         )
